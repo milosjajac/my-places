d8336a60239649440a4d3becdbb43bdaf71a5a9a
Milos Jajac <milosjajac13@gmail.com>
2017-03-21 16:11:09 +0100

    db access methods

--- app/src/main/java/com/example/jajac/myplaces/MyPlacesDBAdapter.java
+++ app/src/main/java/com/example/jajac/myplaces/MyPlacesDBAdapter.java
@@ -2,10 +2,13 @@ package com.example.jajac.myplaces;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by jajac on 3/21/17.
@@ -59,5 +62,83 @@ public class MyPlacesDBAdapter {
             db.endTransaction();
         }
         return id;
+    }
+
+    public boolean removeEntry(long id) {
+        boolean success = false;
+        db.beginTransaction();
+        try {
+            success = db.delete(DATABASE_TABLE, PLACE_ID + "=" + id, null) > 0;
+            db.setTransactionSuccessful();
+        } catch (SQLiteException e) {
+            Log.v("MyPlacesDBAdapter", e.getMessage());
+        } finally {
+            db.endTransaction();
+        }
+        return success;
+    }
+
+    public ArrayList<MyPlace> getAllEntries() {
+        ArrayList<MyPlace> myPlaces = null;
+        Cursor cursor = null;
+        db.beginTransaction();
+        try {
+            cursor = db.query(DATABASE_TABLE, null, null, null, null, null, null);
+        } catch (SQLiteException e) {
+            Log.v("MyPlacesDBAdapter", e.getMessage());
+        } finally {
+            db.endTransaction();
+        }
+
+        if (cursor != null) {
+            myPlaces = new ArrayList<MyPlace>();
+            MyPlace myPlace = null;
+            while (cursor.moveToNext()) {
+                myPlace = new MyPlace(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_NAME)));
+                myPlace.setID(cursor.getLong(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_ID)));
+                myPlace.setDescription(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_DESCRIPTION)));
+                myPlace.setLongitude(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_LONG)));
+                myPlace.setLatitude(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_LAT)));
+                myPlaces.add(myPlace);
+            }
+        }
+        return myPlaces;
+    }
+
+    public MyPlace getEntry(long id) {
+        MyPlace myPlace = null;
+        Cursor cursor = null;
+        db.beginTransaction();
+        try {
+            cursor = db.query(DATABASE_TABLE, null, PLACE_ID + "=" + id, null, null, null, null);
+        } catch (SQLiteException e) {
+            Log.v("MyPlacesDBAdapter", e.getMessage());
+        } finally {
+            db.endTransaction();
+        }
+
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                myPlace = new MyPlace(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_NAME)));
+                myPlace.setID(cursor.getLong(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_ID)));
+                myPlace.setDescription(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_DESCRIPTION)));
+                myPlace.setLongitude(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_LONG)));
+                myPlace.setLatitude(cursor.getString(cursor.getColumnIndex(MyPlacesDBAdapter.PLACE_LAT)));
+            }
+        }
+        return myPlace;
+    }
+
+    public int updateEntry(long id, MyPlace myPlace) {
+        String where = PLACE_ID + "=" + id;
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(PLACE_NAME, myPlace.getName());
+        contentValues.put(PLACE_DESCRIPTION, myPlace.getDesc());
+        contentValues.put(PLACE_LONG, myPlace.getLongitude());
+        contentValues.put(PLACE_LAT, myPlace.getLatitude());
+
+        return db.update(DATABASE_TABLE, contentValues, where, null);
     }
 }
